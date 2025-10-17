8eae9b7a01adc786e18319993e36ab8dd4eb3258
Skyffyy <skyffiriks@gmail.com>
2025-10-17 11:20:31 +0300

    Remove comment about database functions

--- headers/database.h
+++ headers/database.h
@@ -5,7 +5,6 @@
 
 #define DATABASE_FILE "database.txt"
 
-// Database functions
 void loadDatabase(struct Student students[], struct Parent parents[], int *studentCount, int *parentCount);
 void saveDatabase(struct Student students[], struct Parent parents[], int studentCount, int parentCount);
 
