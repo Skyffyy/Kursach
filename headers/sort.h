9e7963eb789b0d30a7ef12db2ee7b8af21fa067c
Skyffyy <skyffiriks@gmail.com>
2025-10-17 11:13:35 +0300

    sort.h

--- headers/sort.h
+++ headers/sort.h
@@ -3,8 +3,18 @@
 
 #include "student.h"
 
+// Student sorting
 void sortByName(struct Student students[], int count);
+void sortBySurname(struct Student students[], int count);
 void sortByBirthYear(struct Student students[], int count);
-void sortByGrade(struct Student students[], int count);
+void sortByAverageGrade(struct Student students[], int count);
+
+// Parent sorting
+void sortParentsByName(struct Parent parents[], int count);
+void sortParentsBySurname(struct Parent parents[], int count);
+void sortParentsByID(struct Parent parents[], int count);
+
+// Utility
+int strCompare(const char *a, const char *b);
 
 #endif
