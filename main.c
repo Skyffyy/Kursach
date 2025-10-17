dfa26468f41e28c8c9b0d8868750df5fa0f0ff23
Skyffyy <skyffiriks@gmail.com>
2025-10-17 12:01:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -19,15 +19,6 @@ void showHelp() {
     printf("  ./main dynamic\n");
 }
 
-// Compare strings using your strEqual function
-int strEqual(const char *a, const char *b){
-    int i = 0;
-    while(a[i] && b[i]){
-        if(a[i] != b[i]) return 0;
-        i++;
-    }
-    return a[i] == b[i];
-}
 
 int main(int argc, char *argv[]){
     if(argc < 2){
