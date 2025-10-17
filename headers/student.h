19914cb7a7c97b828d830576c50c68f0998bdfe0
Skyffyy <skyffiriks@gmail.com>
2025-10-17 11:20:03 +0300

    Update student.h

--- headers/student.h
+++ headers/student.h
@@ -1,16 +1,16 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#define MAX_NAME 100
+#define MAX_NAME 50
 #define MAX_STUDENTS 100
 #define MAX_PARENTS 100
+#define MAX_PARENTS_PER_STUDENT 2
 
 // Parent structure
 struct Parent {
-    int id;
     char name[MAX_NAME];
     char surname[MAX_NAME];
-    char personalID[20];
+    char personalID[20];  // unique identifier
     char email[100];
     char phone[20];
     char birthDate[20];
@@ -19,36 +19,34 @@ struct Parent {
 
 // Student structure
 struct Student {
-    int id;
     char name[MAX_NAME];
     char surname[MAX_NAME];
-    char personalID[20];
+    char personalID[20];   // unique identifier
     char email[100];
     char phone[20];
     char birthDate[20];
     char gender[10];
+
+    float averageGrade;
+    int birthYear;
+
     int parentCount;
-    int parentIDs[2];
+    char parentPersonalIDs[MAX_PARENTS_PER_STUDENT][20]; // links to parents
 };
 
-// Static array operations 
+// Utility
+int strEqual(const char *a, const char *b);
+
+// Static array operations
 void listStudentsStatic(struct Student students[], struct Parent parents[], int studentCount, int parentCount);
 void addStudentStatic(struct Student students[], struct Parent parents[], int *studentCount, int *parentCount);
 void deleteStudentStatic(struct Student students[], int *studentCount);
 
-//  Dynamic array operations 
+// Dynamic array operations
 struct Student* createStudentArray(int initialCapacity);
 void freeStudentArray(struct Student *students);
 void addStudentDynamic(struct Student **students, struct Parent **parents, int *studentCount, int *parentCount, int *capacity);
 void deleteStudentDynamic(struct Student **students, int *studentCount);
 void listStudentsDynamic(struct Student *students, struct Parent *parents, int studentCount, int parentCount);
-
-//Wrappers 
-void listStudents(struct Student *students, struct Parent *parents, int studentCount, int parentCount);
-void addStudent(struct Student *students, struct Parent *parents, int *studentCount, int *parentCount, int dynamic, int *capacity);
-void deleteStudent(struct Student *students, int *studentCount, int *parentCount, int dynamic);
-
-// Utility 
-int strEqual(const char *a, const char *b);
 
 #endif
