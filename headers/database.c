dcb1c1239ae210f1e7a4dfa01eb1794fc099b5d9
Skyffyy <skyffiriks@gmail.com>
2025-10-17 11:20:57 +0300

    Update database.c

--- headers/database.c
+++ headers/database.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include "database.h"
 
-void loadDatabase(struct Student students[], struct Parent parents[], int *studentCount, int *parentCount) {
-    FILE *f = fopen(DATABASE_FILE, "r");
-    if (!f) {
-        f = fopen(DATABASE_FILE, "w");
-        if (!f) { printf("Failed to create file %s\n", DATABASE_FILE); *studentCount=0; *parentCount=0; return; }
+void loadDatabase(struct Student students[], struct Parent parents[], int *studentCount, int *parentCount){
+    FILE *f = fopen(DATABASE_FILE,"r");
+    if(!f){
+        f = fopen(DATABASE_FILE,"w");
+        if(!f){ printf("Cannot create database file.\n"); *studentCount=0; *parentCount=0; return;}
         fclose(f);
-        printf("File %s not found. Created empty database.\n", DATABASE_FILE);
         *studentCount = 0;
         *parentCount = 0;
         return;
@@ -18,44 +17,44 @@ void loadDatabase(struct Student students[], struct Parent parents[], int *stude
     *parentCount = 0;
     char type;
     while(fscanf(f," %c",&type)==1){
-        if(type=='S'){
-            struct Student *s = &students[*studentCount];
-            int p1=-1,p2=-1;
-            fscanf(f,"%d %99s %99s %19s %99s %19s %19s %9s %d %d %d",
-                   &s->id, s->name, s->surname, s->personalID, s->email, s->phone,
-                   s->birthDate, s->gender, &s->parentCount, &p1, &p2);
-            s->parentIDs[0] = p1;
-            s->parentIDs[1] = p2;
-            (*studentCount)++;
-        } else if(type=='P'){
+        if(type=='P'){
             struct Parent *p = &parents[*parentCount];
-            fscanf(f,"%d %99s %99s %19s %99s %19s %19s %9s",
-                   &p->id, p->name, p->surname, p->personalID, p->email, p->phone,
-                   p->birthDate, p->gender);
+            fscanf(f,"%49s %49s %19s %99s %19s %19s %9s",
+                   p->name, p->surname, p->personalID, p->email, p->phone, p->birthDate, p->gender);
             (*parentCount)++;
         }
+        else if(type=='S'){
+            struct Student *s = &students[*studentCount];
+            s->parentCount = 0;
+            fscanf(f,"%49s %49s %19s %99s %19s %19s %9s %f %d %d",
+                   s->name, s->surname, s->personalID, s->email, s->phone,
+                   s->birthDate, s->gender, &s->averageGrade, &s->birthYear, &s->parentCount);
+            for(int i=0;i<s->parentCount;i++)
+                fscanf(f,"%19s", s->parentPersonalIDs[i]);
+            (*studentCount)++;
+        }
     }
-
     fclose(f);
 }
 
-void saveDatabase(struct Student students[], struct Parent parents[], int studentCount, int parentCount) {
-    FILE *f = fopen(DATABASE_FILE, "w");
-    if (!f){ printf("Failed to save file %s\n", DATABASE_FILE); return; }
+void saveDatabase(struct Student students[], struct Parent parents[], int studentCount, int parentCount){
+    FILE *f = fopen(DATABASE_FILE,"w");
+    if(!f){ printf("Cannot save database file.\n"); return; }
 
-    // parents
     for(int i=0;i<parentCount;i++){
         struct Parent p = parents[i];
-        fprintf(f,"P %d %s %s %s %s %s %s %s\n",
-                p.id, p.name, p.surname, p.personalID, p.email, p.phone, p.birthDate, p.gender);
+        fprintf(f,"P %s %s %s %s %s %s %s\n",
+                p.name, p.surname, p.personalID, p.email, p.phone, p.birthDate, p.gender);
     }
 
-    // students
     for(int i=0;i<studentCount;i++){
         struct Student s = students[i];
-        fprintf(f,"S %d %s %s %s %s %s %s %s %d %d %d\n",
-                s.id, s.name, s.surname, s.personalID, s.email, s.phone, s.birthDate, s.gender,
-                s.parentCount, s.parentIDs[0], s.parentIDs[1]);
+        fprintf(f,"S %s %s %s %s %s %s %s %.2f %d %d",
+                s.name, s.surname, s.personalID, s.email, s.phone,
+                s.birthDate, s.gender, s.averageGrade, s.birthYear, s.parentCount);
+        for(int j=0;j<s.parentCount;j++)
+            fprintf(f," %s", s.parentPersonalIDs[j]);
+        fprintf(f,"\n");
     }
 
     fclose(f);
